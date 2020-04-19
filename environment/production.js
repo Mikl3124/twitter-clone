98b20d7bbf103e5f6451812ca93901885a93e5a8
Mikl3124 <mickael.delpech@gmail.com>
2020-04-20 01:58:44 +0200

    fix

--- environment/production.js
+++ environment/production.js
@@ -1,5 +1,5 @@
 module.exports = {
     dbUrl: 'mongodb+srv://Mikl3124:Mickael31!@cluster0-rnoqc.mongodb.net/twitter?retryWrites=true&w=majority',
-    cert: '',
-    key: '',
-  }
+    cert: '/etc/letsencrypt/live/www.kazatravo.com/fullchain.pem',
+    key: '/etc/letsencrypt/live/www.kazatravo.com/privkey.pem'
+}
