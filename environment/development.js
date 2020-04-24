4be68949eb6445db2724203736b841cba91cf94a
Mikl3124 <mickael.delpech@gmail.com>
2020-04-24 11:09:51 +0200

    push 2

--- environment/development.js
+++ environment/development.js
@@ -1,7 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  dbUrl: 'mongodb+srv://Mikl3124:Mickael31!@cluster0-rnoqc.mongodb.net/twitter?retryWrites=true&w=majority',
   cert: path.join( __dirname, '../ssl/local.crt'),
   key: path.join( __dirname, '../ssl/local.key'),
   portHttp:3000,
--- environment/production.js
+++ environment/production.js
@@ -1,5 +1,4 @@
 module.exports = {
-    dbUrl: 'mongodb+srv://Mikl3124:Mickael31!@cluster0-rnoqc.mongodb.net/twitter?retryWrites=true&w=majority',
     cert: '/etc/letsencrypt/live/www.kazatravo.com/fullchain.pem',
     key: '/etc/letsencrypt/live/www.kazatravo.com/privkey.pem',
     portHttp:80,
